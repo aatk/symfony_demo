4615b12fed89fee7f0dc5c3433b56733604b96ae
aleksandr <at@zerotech.ru>
2021-02-10 20:16:52 +0300

    [symfony demo]
    migration

--- migrations/Version20210210171031.php
+++ migrations/Version20210210171031.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210207073125 extends AbstractMigration
+final class Version20210210171031 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -21,11 +21,13 @@ final class Version20210207073125 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(70) DEFAULT NULL, secondname VARCHAR(70) DEFAULT NULL, surname VARCHAR(70) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE FULLTEXT INDEX fulltextindex ON users (firstname, secondname, surname)');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX fulltextindex ON users');
         $this->addSql('DROP TABLE users');
     }
 }
